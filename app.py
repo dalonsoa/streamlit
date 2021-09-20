e442e8bdacf786b3f431a276ff494ae8859f4f66
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-09-20 10:41:46 +0100

    Add some widgets

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 import streamlit as st
 
-st.title('My first Streamlit app')
+st.title("My first Streamlit app. It's cool! Or not.")
+
+st.checkbox('Tick me')
+
+if st.checkbox('Tick me to see'):
+  st.write(42)
+
+
+selection = st.selectbox('Choose an option', ["Nothing here", "or here", "but something here"])
+
+if selection == "but something here":
+  st.write("You found me!")
+else:
+  st.write("Try again")
+
+
+int_val = st.slider("Pick an integer", min_value=1, max_value=10, value=5)
+st.write(int_val)
+
+
+float_val = st.slider("Pick a float", min_value=1.0, max_value=10.0, value=5.0, step=0.1, format="%.1f")
+st.write(float_val)
+
+
+st.sidebar.write("Adding text to the sidebar...")
