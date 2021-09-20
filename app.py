94626c5490459bfe8a9d7559180dc2319564853f
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-09-20 11:24:57 +0100

    Add statistics and move things to modules

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 import streamlit as st
 
-st.title("My first Streamlit app. It's cool! Or not.")
-
-st.checkbox('Tick me')
-
-if st.checkbox('Tick me to see'):
-  st.write(42)
+from distributions import normal_distribution
 
 
-selection = st.selectbox('Choose an option', ["Nothing here", "or here", "but something here"])
+st.title('Statistical distributions')
 
-if selection == "but something here":
-  st.write("You found me!")
-else:
-  st.write("Try again")
+dist_selector = st.sidebar.selectbox(
+    "Pick a distribution:",
+    ("Beta", "Normal"),
+    index=1
+)
 
+st.header(dist_selector)
 
-int_val = st.slider("Pick an integer", min_value=1, max_value=10, value=5)
-st.write(int_val)
-
-
-float_val = st.slider("Pick a float", min_value=1.0, max_value=10.0, value=5.0, step=0.1, format="%.1f")
-st.write(float_val)
-
-
-st.sidebar.write("Adding text to the sidebar...")
+if dist_selector == 'Normal':
+    normal_distribution()
